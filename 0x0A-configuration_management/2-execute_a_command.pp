d2be699acd1397ad27705e2ca9aaa16188c01b63
Diego Tardio <diegotardio@Diegos-MBP.home>
2020-03-23 20:26:27 -0400

    [ADDED PATH]

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,5 +1,5 @@
 # Using Puppet, create a manifest that kills a process named killmenow.
 exec { 'killmenow':
-  command  => 'pkill -f killmenow',
+  command  => '/usr/bin/pkill -f killmenow',
   provider => 'shell',
 }
