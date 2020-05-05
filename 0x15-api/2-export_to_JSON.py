7982b54b3835a9611dce34337fc7e825ee8606f5
Diego Tardio <diegotardio@Diegos-MBP.home>
2020-05-04 20:56:09 -0400

    [TESTING JSON FORMAT]

---
+++ 0x15-api/2-export_to_JSON.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""export data in the JSON format."""
+
+if __name__ == '__main__':
+    import json
+    import requests
+    import sys import argv
+
+    Ag = sys.argv[1]
+
+    """Getting the Tasks."""
+    ApiUrl = "https://jsonplaceholder.typicode.com/todos?userId="
+    Response = requests. get(ApiUrl + Ag)
+    ListOfTasks = Response.json()
+
+    """Getting the Users Name."""
+    ApiUrl = "https://jsonplaceholder.typicode.com/users/"
+    Response = requests.get(ApiUrl + Ag)
+    Name = Response.json()
+    Uname = Name.get("username")
+
+    Data = {}
+    DataList = []
+
+    for task in ListOfTasks:
+        Data["task"] = task.get("title")
+        Data["completed"] = task.get("completed")
+        Data["username"] = Uname
+        Data.append(DataList)
+        DataList = {}
+
+    ExportJson = {}
+    ExportJson[Ag] = DataList
+
+    with open(Ag + ".json", "w") as JsonFormat:
+        json.dump(ExportJson, JsonFormat)
