825c390950a1a624b47400b990366130bb9eb02d
Diego Tardio <diegotardio@Diegos-MBP.home>
2020-05-07 22:49:32 -0400

    [BUG FIX] PEP8 checked for white space

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -6,7 +6,7 @@ from requests import get
 def number_of_subscribers(subreddit):
     """Return the amount of subscribers."""
     url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
-    Headers = {'User-Agent':'Agent-Subscribe'}
+    Headers = {'User-Agent': 'Agent-Subscribe'}
     Response = get(url, headers=headers, allow_redirects=False)
     if (Response.status_code != 200):
         return 0
