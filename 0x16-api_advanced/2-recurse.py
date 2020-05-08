14a9c469f115b097c50e3fa7706758f10b785f9e
Diego Tardio <diegotardio@Diegos-MBP.home>
2020-05-07 23:48:32 -0400

    [ADDED PARAMETER]

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -6,17 +6,16 @@ import requests import get
 def recurse(subreddit, hot_list=[], after=None):
     """Reddit API uses pagination for separating pages of responses."""
     url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
-    user_agent = {'User-Agent': 'Agent-Subscribe'}
-    Response = get(url, headers=user_agent,
+    Agents = {'User-Agent': 'Agent-Subscribe'}
+    Response = get(url, headers=Agents,
                    allow_redirects=False, params={"after": after})
     if Response.status_code != 200:
         return None
-    else:
-        ChildTitles = Response.json()['data']['children']
-        AfterData = Response.json()['data']['after']
 
-        for HotArticles in ChildTitles:
-            hot_list.append(HotArticles['data']['title'])
-        if AfterData is None:
-            return hot_list
-        return recurse(subreddit, hot_list, AfterData)
+    ChildTitles = Response.json()['data']['children']
+    AfterData = Response.json()['data']['after']
+    for HotArticles in ChildTitles:
+        hot_list.append(HotArticles['data']['title'])
+    if AfterData is None:
+        return hot_list
+    return recurse(subreddit, hot_list, AfterData)
