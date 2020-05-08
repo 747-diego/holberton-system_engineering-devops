6bb9e65fcf0465ed9b7cb21d3693981787ef8eb5
Diego Tardio <diegotardio@Diegos-MBP.home>
2020-05-07 23:42:34 -0400

    [ADDE PARAMETER]

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -8,7 +8,7 @@ def recurse(subreddit, hot_list=[], AfterData=None):
     url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
     user_agent = {'User-Agent': 'Agent-Subscribe'}
     Response = get(url, headers=user_agent,
-                   allow_redirects=False, params={"AfterData": AfterData})
+                   allow_redirects=False, params={"after": AfterData})
     if Response.status_code != 200:
         return None
     else:
