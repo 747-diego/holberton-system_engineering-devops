e58266daf2689a3459b2bff8a79005019ff62d7e
Diego Tardio <diegotardio@Diegos-MBP.home>
2020-05-28 23:39:51 -0400

    [TASK 0. Sky is the limit, let's bring that limit higher]

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,5 +1,5 @@
 # Testing web server setup using Nginx
 exec { 'TESTING Web Server':
-  command  => 'sudo sed -i "s/15/4096/g" /etc/default/nginx; sudo service nginx restart',
+  command  => 'sed -i "s/15/4096/g" /etc/default/nginx; sudo service nginx restart',
   provider => shell,
 }
